2b2cb99aff0726eba3b5f8040dad18769e1d1bd2
Huzaifa Tariq <huzaifa.ariatech@gmail.com>
2026-04-09 00:49:55 +0500

    Update 404 error page text for clarity and consistency

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -18,11 +18,9 @@
             <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404">
             <span>404 PAGE</span>
             <p class="p-a">
-                . The page you were looking for could not be found</p>
-            <p class="p-b">
-                ... Back to previous page
+                The page you were looking for could not be found
             </p>
-            <a href='/' class="back">... Back to previous page</a>
+            <a href='/' class="back">Back To Home Page</a>
         </div>
     </div>
 
