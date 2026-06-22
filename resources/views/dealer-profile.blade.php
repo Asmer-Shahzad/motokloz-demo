bfd42930999f3657fb3d8159e8f81d6dc24008cc
huzaifaariatech-ariatech <huzaifa.ariatech@gmail.com>
2026-06-23 03:55:18 +0500

    fix

--- resources/views/dealer-profile.blade.php
+++ resources/views/dealer-profile.blade.php
@@ -181,6 +181,13 @@ $isMotokloz = $source === 'motokloz';
 
 
                     </div>
+                    <div class="mb-4">
+                        @if(!empty($dealer->internal_notes))
+                        <p>{{ $dealer->internal_notes }}</p>
+                        @else
+                        <p class="text-muted">No description available for this dealer.</p>
+                        @endif
+                    </div>
 
                 </div>
             </div>
